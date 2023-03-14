31a3cb1dbe28a0106e4e1d60dcb465974afa57ff
nftchance <chance@utc24.io>
2023-03-14 12:44:17 -0500

    feat: query formatting

--- queries/nuclear_nerds/mint_revenue_sources.sql
+++ queries/nuclear_nerds/mint_revenue_sources.sql
@@ -1,19 +1,10 @@
-select
-  nft_to_address as minter_wallet,
-  count(tokenid) as n_minted_nft,
-  count(DISTINCT tx_hash) as n_mint_txns,
-  sum(mint_price_eth) as total_eth_spent
-from
-  ethereum.core.ez_nft_mints
-where
-  nft_address = LOWER('0x026234c69cdfa4dc0c7f01806df6b9d63e238b80')
-  OR nft_address = LOWER('0x120305aace78052a967182b277f6392670ee9873') 
-  OR nft_address = LOWER('0x072d62047b03b9ee68596557aee848188422150b')
-group by
-  minter_wallet
-having
-  total_eth_spent > 0
-order by
-  total_eth_spent DESC
-LIMIT
-  100
+SELECT  nft_to_address          AS minter_wallet
+       ,COUNT(tokenid)          AS n_minted_nft
+       ,COUNT(DISTINCT tx_hash) AS n_mint_txns
+       ,SUM(mint_price_eth)     AS total_eth_spent
+FROM ethereum.core.ez_nft_mints
+WHERE nft_address = LOWER('0x026234c69cdfa4dc0c7f01806df6b9d63e238b80') OR nft_address = LOWER('0x120305aace78052a967182b277f6392670ee9873') OR nft_address = LOWER('0x072d62047b03b9ee68596557aee848188422150b')
+GROUP BY  minter_wallet
+HAVING total_eth_spent > 0
+ORDER BY total_eth_spent DESC
+LIMIT 100
